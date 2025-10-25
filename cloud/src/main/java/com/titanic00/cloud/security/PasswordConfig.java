115552ddfb7141e89d0833803c43a8f889620310
Vitaliy_Mi <vitalii.mishchenko@cec.valantic.com>
2025-10-25 21:23:07 +0200

    add dockerignore to backend, import password encoder package

--- cloud/src/main/java/com/titanic00/cloud/security/PasswordConfig.java
+++ cloud/src/main/java/com/titanic00/cloud/security/PasswordConfig.java
@@ -3,12 +3,13 @@ package com.titanic00.cloud.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class PasswordConfig {
 
     @Bean
-    org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
